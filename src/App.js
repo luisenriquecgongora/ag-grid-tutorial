43917192cf2e24edcebc5bd564c6b578ea1fbef8
Luis <luisenriquecgongora@gmail.com>
2020-09-09 18:52:54 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import 'ag-grid-enterprise';
 
-function App() {
+const App = () => {
+
+  const [gridApi, setGridApi] = useState();
+
+  const columnDefs = [{
+    headerName: "Make", field: "make", sortable: true, filter: true, checkboxSelection: true
+  }, {
+    headerName: "Model", field: "model", 
+  }, {
+    headerName: "Price", field: "price"
+  }];
+
+  const rowData = [{
+    orgHierarchy: ['PELI123'], make: "Toyota", model: "Celica", price: 35000
+  }, {
+    orgHierarchy: ['PELI123', 'Draft'], make: "Ford", model: "Mondeo", price: 32000
+  }, {
+    orgHierarchy: ['LC', 'KN'], make: "Porsche", model: "Boxter", price: 72000
+  }, {
+    orgHierarchy: ['Erica Rogers', 'KYC'], make: "Mercedes", model: "Mondeo", price: 32000 
+  }
+];
+
+  const autoGroupColumnDef = {
+    headerName: "Organization",
+    cellRenderer: 'agGroupCellRenderer',
+    cellRendererParams: {
+      checkbox: true
+    }
+  };
+
+  
+  const handleGridReady = (params) => {
+    setGridApi(params.api)
+  }
+
+  const handleButtonClick = () => {
+    const selectedNodes = gridApi.getSelectedNodes();
+    console.log("Selected Nodes", selectedNodes);
+    const data = selectedNodes.map((node) => node.data);
+    console.log("Data> ", data);
+  }
+
+  const getDataPath = (data)=> {
+      return data.orgHierarchy;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="ag-theme-alpine"
+      style={{
+        height: '250px',
+        width: '600px'
+      }}
+    >
+      <AgGridReact
+        onGridReady={handleGridReady}
+        rowSelection="multiple"
+        columnDefs={columnDefs}
+        groupSelectsChildren={true}
+        treeData={true  }
+        autoGroupColumnDef={autoGroupColumnDef}
+        getDataPath={getDataPath}
+        rowData={rowData}>
+      </AgGridReact>
+      <button onClick={handleButtonClick}>Export Data</button>
     </div>
   );
 }
